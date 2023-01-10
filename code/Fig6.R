d1e1ac523038c6036262f8c7d281f8479938be7b
Your Name <you@example.com>
2023-01-10 14:27:45 +0900

    Figure6 legend

--- code/Fig6.R
+++ code/Fig6.R
@@ -10,22 +10,22 @@ source("code/Packages_to_load.R")
 #read png convert to image panel
 
 panel_scRNA_dotplot <- ggdraw() + draw_image(readPNG("pictures/scRNAseq_forR_5.png"))
-panel_model <- ggdraw() + draw_image(readPNG("pictures/model.png"))
+panel_model <- ggdraw() + draw_image(readPNG("pictures/model_2.png"))
 
 #combine panels into Figure and save final figure as pdf and png
 #panels of different sizes
 layout <- "
-A#B
+AB
 "
 
 Fig6 <- panel_scRNA_dotplot + panel_model +
   patchwork::plot_layout(design = layout, 
-                         widths = c(1, 0.05, 2)) + #we can change the heights of the rows in our layout (widths also can be defined)
+                         widths = c(1, 1)) + #we can change the heights of the rows in our layout (widths also can be defined)
   patchwork::plot_annotation(tag_levels = 'A') +  #we can change this to 'a' for small caps or 'i' or '1'
   ggplot2::theme(plot.tag = element_text(size = 12, face='bold')) #or 'bold', 'italic'
 
 ggsave("figures/Fig6.png", limitsize = FALSE, 
-       units = c("px"), Fig6, width = 2400, height = 1000, bg='white')  
+       units = c("px"), Fig6, width = 1800, height = 1000, bg='white')  
 
 ggsave("figures/Fig6.pdf", limitsize = FALSE, 
-       units = c("px"), Fig6, width = 2400, height = 1000)  
+       units = c("px"), Fig6, width = 1800, height = 1000)  
