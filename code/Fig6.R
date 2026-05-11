fd5e2ada8c80b51f9263c214dc7225ef054ea6ba
Gaspar Jekely <gaspar.jekely@cos.uni-heidelberg..de>
2026-05-11 08:39:40 +0200

    ms: edit math model, add Fig6 supplements, Fig6 update

--- code/Fig6.R
+++ code/Fig6.R
@@ -1,5 +1,5 @@
 #Code to assemble Fig6 of the Jokura et al Platynereis NOS paper
-#2022 June - Kei Jokura, Gaspar Jekely
+# 2022 June - Kei Jokura, Gaspar Jekely
 
 # source packages ---------------------------------------------------------
 source("code/Packages_to_load.R")
@@ -11,28 +11,43 @@ source("code/Packages_to_load.R")
 
 panel_scRNA_dotplot <- ggdraw() + draw_image(readPNG("pictures/scRNAseq.png"))
 panel_model <- ggdraw() + draw_image(readPNG("pictures/model_2.png"))
-
-panel_fitting <- ggdraw() + draw_image(readPNG("pictures/fitting_data.png"))
-panel_block_diagram <- ggdraw() + draw_image(readPNG("pictures/fitting_data_block_diagram.png"))
+panel_block_diagram <- ggdraw() +
+       draw_image(readPNG("pictures/model_block_diagram_v7.png"))
 
 #combine panels into Figure and save final figure as pdf and png
 #panels of different sizes
 layout <- "
-A#B
+AAA
 ###
-C#D
+B#C
 "
 
-Fig6 <-panel_block_diagram + panel_fitting + 
-  panel_scRNA_dotplot + panel_model + 
-  patchwork::plot_layout(design = layout, 
-                         heights = c(1, 0.05, 1),
-                         widths = c(1,0.05, 1)) + #we can change the heights of the rows in our layout (widths also can be defined)
-  patchwork::plot_annotation(tag_levels = 'A') &  #we can change this to 'a' for small caps or 'i' or '1'
-  ggplot2::theme(plot.tag = element_text(size = 12, face='plain')) #or 'plain', 'italic'
+Fig6 <- panel_block_diagram +
+       panel_scRNA_dotplot +
+       panel_model +
+       patchwork::plot_layout(
+              design = layout,
+              heights = c(1, 0.05, 1),
+              widths = c(1, 0.05, 1)
+       ) + #we can change the heights of the rows in our layout (widths also can be defined)
+       patchwork::plot_annotation(tag_levels = 'A') & #we can change this to 'a' for small caps or 'i' or '1'
+       ggplot2::theme(plot.tag = element_text(size = 12, face = 'plain')) #or 'plain', 'italic'
 
-ggsave("Manuscript/figures/Fig6.png", limitsize = FALSE, 
-       units = c("px"), Fig6, width = 1800, height = 2200, bg='white')  
+ggsave(
+       "Manuscript/figures/Fig6.png",
+       limitsize = FALSE,
+       units = c("px"),
+       Fig6,
+       width = 1800,
+       height = 2200,
+       bg = 'white'
+)
 
-ggsave("Manuscript/figures/Fig6.pdf", limitsize = FALSE, 
-       units = c("px"), Fig6, width = 1800, height = 2200)  
+ggsave(
+       "Manuscript/figures/Fig6.pdf",
+       limitsize = FALSE,
+       units = c("px"),
+       Fig6,
+       width = 1800,
+       height = 2200
+)
