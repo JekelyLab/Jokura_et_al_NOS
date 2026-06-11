4c953592fcee068e9f922204cf54373196d2287a
Gaspar Jekely <gaspar.jekely@cos.uni-heidelberg..de>
2026-06-11 09:08:23 +0200

    NOS mutants: add lunar cycle data as Fig3 sup4, with code, plot and source data

---
+++ code/Fig3_sup4.R
@@ -0,0 +1,77 @@
+#Code to plot NOS mutant and wt mature worm stats for the Jokura et al Platynereis NOS paper
+
+# source packages ---------------------
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+Okabe_Ito <- c(
+  "#E69F00",
+  "#56B4E9",
+  "#009E73",
+  "#F0E442",
+  "#0072B2",
+  "#D55E00",
+  "#CC79A7",
+  "#000000"
+)
+
+# load data ---------------------------------------------------
+
+NOS_stats <- tibble::as_tibble(rio::import(
+  "data/NOS_mutant_and_WT_mature_animals.csv"
+))
+
+# tidy the data -----------------
+
+NOS_stats_tidy <- NOS_stats |>
+  mutate(DATE = lubridate::as_date((DATE))) |>
+  pivot_longer(cols = contains("_"),
+    names_to = c("genotype", "sex"),
+    names_sep = "_",
+    values_to = "mature_worms"
+    ) |>
+  rename(date = DATE) |>
+  mutate(sex = case_when(sex == 'F' ~ 'female',
+                        sex == 'M' ~ 'male')
+         )
+
+
+NOS_stats_tidy
+#plotting ----------------
+
+NOS_stats_plot <- NOS_stats_tidy |>
+  group_by(genotype, sex) |>
+  ggplot() +
+  geom_line(aes(date, mature_worms), size = 0.3) +
+  geom_smooth(
+    aes(date, mature_worms), 
+    span = 0.01, size = 0.3, 
+    na.rm = TRUE
+    ) +
+  facet_grid(~genotype ~sex) +
+  scale_y_continuous(limits = c(0, 40)) +
+  labs(y = "mature worms") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90))
+
+NOS_stats_plot
+
+ggsave(
+  "Manuscript/figures/Fig3_sup4.png",
+  limitsize = FALSE,
+  units = c("px"),
+  NOS_stats_plot,
+  width = 2200,
+  height = 1400,
+  bg = 'white'
+)
+
+
+ggsave(
+  "Manuscript/figures/Fig3_sup4.pdf",
+  limitsize = FALSE,
+  units = c("px"),
+  NOS_stats_plot,
+  width = 2200,
+  height = 1400
+)
